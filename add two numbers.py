feaf11b08cf81f85f986c8c45c42d0fc31d1c61c
Saad <msaadmunirbhatti@gmail.com>
2026-01-22 21:33:57 +0500

    Add files via upload

---
+++ add two numbers.py
@@ -0,0 +1,9 @@
+# Get two numbers from user input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {sum}")
